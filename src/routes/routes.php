33dcb4b532aa36fc495a8d59cb3298256cb9fb9b
Joshua Kissoon <joshuakissoon@gmail.com>
2018-04-09 13:31:54 -0400

    Fixed a bug with routes

--- src/routes/routes.php
+++ src/routes/routes.php
@@ -2,8 +2,8 @@
 
 Route::group(['prefix' => 'api', 'middleware' => 'api'], function()
 {
-    Route::resource("users", "User\UserController");
-    Route::post('/user/logout', "User\SessionController@destroy");
-    Route::get('/user/current', "User\CurrentUserController@show");
-    Route::patch("user/current/update-password", "User\UserController@user_password_change_own");
+    Route::resource("users", "TechlifyInc\LaravelUserManagement\Controllers\UserController");
+    Route::post('/user/logout', "TechlifyInc\LaravelUserManagement\Controllers\SessionController@destroy");
+    Route::get('/user/current', "TechlifyInc\LaravelUserManagement\Controllers\CurrentUserController@show");
+    Route::patch("user/current/update-password", "TechlifyInc\LaravelUserManagement\Controllers\UserController@user_password_change_own");
 });
